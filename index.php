f5f42a30d32c2eb73ed79ee19c3de5e99b1d85b0
saikishor164 <rasalasaikishor@gmail.com>
2022-11-04 14:42:31 +0530

    create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resume</title>
+</head>
+<body>
+    <h1>Saikishor Rasala</h1>
+    <h2>Front End Web Developer</h2>
+</body>
+</html>
