4ff8479d54401ca2b2220993dbc05410018dfb68
Saikishor Rasala <rasalasaikishor@gmail.com>
2023-10-20 15:50:56 +0530

    deleted react project

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    fontFamily: {
+      primary: 'Orbitron',
+      secondary: 'Rajdhani',
+      tertiary: 'Aldrich',
+    },
+    container: {
+      padding: {
+        DEFAULT: '15px',
+      },
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '960px',
+      xl: '1200px',
+    },
+    extend: {
+      colors: {
+        primary: '#0a0a0a',
+        accent: '#B809C3',
+      },
+      backgroundImage: {
+        site: "url('./assets/site-bg.jpg')",
+        about: "url('./assets/about.png')",
+        services: "url('./assets/services.png')",
+      },
+    },
+  },
+  plugins: [],
+};
